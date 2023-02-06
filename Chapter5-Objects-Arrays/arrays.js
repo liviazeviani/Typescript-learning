9cbf5a08c5b73c103772304f8414d802622e1347
liviapz <liviapavinizeviani@gmail.com>
2023-02-06 16:51:31 -0300

    de-structuring array

--- Chapter5-Objects-Arrays/arrays.js
+++ Chapter5-Objects-Arrays/arrays.js
@@ -1,6 +1,10 @@
 var products = ["phone", "MobilePhone", "ChipCard", "line"];
 products.push(9);
-
 for (var i = 0; i < products.length; i++) {
     console.log(products[i]);
 }
+//de-structuring objects
+var a = products[0], b = products[1], c = products[2];
+console.log(a);
+console.log(b);
+console.log(c);
--- Chapter5-Objects-Arrays/arrays.ts
+++ Chapter5-Objects-Arrays/arrays.ts
@@ -5,3 +5,11 @@ products.push(9);
 for(var i = 0; i< products.length; i++){
     console.log(products[i]);
 }
+
+//de-structuring objects
+var [a, b, c] = products;
+console.log(a);
+console.log(b);
+console.log(c);
+
+ 

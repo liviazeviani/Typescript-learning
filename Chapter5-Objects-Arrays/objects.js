17dd38ebf5af9980a2d0c80b4035a0c2ed38e51c
liviapz <liviapavinizeviani@gmail.com>
2023-02-06 13:58:11 -0300

    initializing Chapter 5 with and object

---
+++ Chapter5-Objects-Arrays/objects.js
@@ -0,0 +1,6 @@
+//declaring and acessing an object
+var student = {
+    studentName: "Lívia",
+    age: 30
+};
+console.log(student.studentName);
---
+++ Chapter5-Objects-Arrays/objects.ts
@@ -0,0 +1,9 @@
+//declaring and acessing an object
+
+var student = {
+    studentName : "Lívia",
+    age : 30
+}
+
+console.log(student.studentName);
+

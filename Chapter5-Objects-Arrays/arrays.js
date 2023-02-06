d95a16a0226ab3dc56503ff25f7ffe44210a8ad7
liviapz <liviapavinizeviani@gmail.com>
2023-02-06 16:37:09 -0300

    learning array, how to push and listing elements with a for

---
+++ Chapter5-Objects-Arrays/arrays.js
@@ -0,0 +1,6 @@
+var products = ["phone", "MobilePhone", "ChipCard", "line"];
+products.push(9);
+
+for (var i = 0; i < products.length; i++) {
+    console.log(products[i]);
+}
---
+++ Chapter5-Objects-Arrays/arrays.ts
@@ -0,0 +1,7 @@
+var products: any = ["phone", "MobilePhone", "ChipCard", "line"];
+
+products.push(9);
+
+for(var i = 0; i< products.length; i++){
+    console.log(products[i]);
+}

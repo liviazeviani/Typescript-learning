076c8a57228536f35cc0169e30e538cd8d3e9861
liviapz <liviapavinizeviani@gmail.com>
2023-02-07 17:08:40 -0300

    anonymous function

---
+++ Chapter6-Functions/anonfunction.js
@@ -0,0 +1,9 @@
+function hi(name) {
+    return "Hi" + name;
+}
+console.log(hi(" lívia"));
+//anonymous function
+var hii = function (name) {
+    return "Hi" + name;
+};
+console.log(hii(" lívia"));
---
+++ Chapter6-Functions/anonfunction.ts
@@ -0,0 +1,13 @@
+function hi(name:string):string{
+    return "Hi" + name;
+}
+
+console.log(hi(" lívia"));
+
+//anonymous function
+var hii = function(name:string):string{
+    return "Hi" + name;
+
+}
+
+console.log(hii(" lívia"));

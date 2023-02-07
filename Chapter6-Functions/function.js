eb932961f1feae199845b69900195a4171f48e50
liviapz <liviapavinizeviani@gmail.com>
2023-02-07 00:31:44 -0300

    learning function and some characteristics

---
+++ Chapter6-Functions/function.js
@@ -0,0 +1,8 @@
+function hello(name) {
+    return "Hello" + " " + name;
+}
+console.log(hello("Lívia"));
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log("n1 + n2 = " + add(10, 10));
---
+++ Chapter6-Functions/function.ts
@@ -0,0 +1,12 @@
+function hello(name:string):string{
+    return "Hello" + " " + name;
+}
+
+
+console.log(hello("Lívia"));
+
+function add(n1:number, n2:number): number{
+    return n1 + n2;
+}
+
+console.log("n1 + n2 = " + add(10, 10));

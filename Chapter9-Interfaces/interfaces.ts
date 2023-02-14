180af59454d8fc95c30541d8ae812e8d32bffdf2
liviapz <liviapavinizeviani@gmail.com>
2023-02-14 02:35:27 -0300

    creating object from an interface

---
+++ Chapter9-Interfaces/interfaces.ts
@@ -0,0 +1,13 @@
+interface IProduct{
+    id:number; 
+    name:string;
+    description:string;
+    price:number;
+}
+
+var productA = {
+    id:1,
+    name: "cellphone",
+    description: "IPhone",
+    price: 3400
+}

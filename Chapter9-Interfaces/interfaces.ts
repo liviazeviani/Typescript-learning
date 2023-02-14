a69073888aac2934a7dcc58b9e08d78d7cfb2687
liviapz <liviapavinizeviani@gmail.com>
2023-02-14 02:38:28 -0300

    defining description as an optional property

--- Chapter9-Interfaces/interfaces.ts
+++ Chapter9-Interfaces/interfaces.ts
@@ -1,13 +1,12 @@
 interface IProduct{
     id:number; 
     name:string;
-    description:string;
+    description?:string;
     price:number;
 }
 
 var productA = {
     id:1,
     name: "cellphone",
-    description: "IPhone",
     price: 3400
 }
